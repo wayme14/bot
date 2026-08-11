1885e14d292068601f7114092edcad76e47c60e0
WAY <77503370+wayme14@users.noreply.github.com>
2026-08-11 15:08:39 +0700

    Update extract.js

--- src/extract.js
+++ src/extract.js
@@ -2,7 +2,7 @@ const { chromium } = require("playwright");
 const fs = require("fs");
 
 const TARGET_URL =
-  "https://op-flashcon-digdayahd-1.dens.tv/h/h223/index.m3u8?app_type=web&userid=lite&chname=MDTV";
+  "https://sindikasi.inews.id/embed/video/YWdlbnQ9ZGVza3RvcCZ1cmw9aHR0cHMlM0ElMkYlMkZlbWJlZC5yY3RpcGx1cy5jb20lMkZsaXZlJTJGcmN0aSUyRmluZXdzaWQmaGVpZ2h0PTEwMCUyNSZ3aWR0aD0xMDAlMjU=";
 
 (async () => {
   const browser = await chromium.launch({ headless: true });
