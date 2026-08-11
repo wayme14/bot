04ef402409db33d7c3dd84b7bd7cca097e9f5d6e
WAY <77503370+wayme14@users.noreply.github.com>
2026-08-11 13:53:25 +0700

    Update extract.js

--- src/extract.js
+++ src/extract.js
@@ -2,7 +2,7 @@ const { chromium } = require("playwright");
 const fs = require("fs");
 
 const TARGET_URL =
-  "https://www.vidio.com/live/205-indosiar";
+  "https://op-flashcon-digdayahd-1.dens.tv/h/h223/index.m3u8?app_type=web&userid=lite&chname=MDTV";
 
 (async () => {
   const browser = await chromium.launch({ headless: true });
